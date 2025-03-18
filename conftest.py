12a6abcb9f411c0a89480ed508d90ad9851ea370
irakey <102231888+irakey@users.noreply.github.com>
2025-03-18 16:04:49 +0100

    fix

--- conftest.py
+++ conftest.py
@@ -10,4 +10,11 @@ def create_user():
     yield user_data
     login_user = requests.post(Urls.LOG_IN, data=user_data)
     token = login_user.json()['accessToken']
-    delete_user = requests.delete(Urls.UPDATE_USER, headers={'Authorization': token})
+    requests.delete(Urls.UPDATE_USER, headers={'Authorization': token})
+
+
+@pytest.fixture
+def authorize_user(create_user):
+    login_user = requests.post(Urls.LOG_IN, data=create_user)
+    token = login_user.json()['accessToken']
+    return token
